f4a52799266c725bf502c0a27d1bb9a836375c0b
ShinjungOh <tlswjd9260@gmail.com>
2022-12-16 22:50:48 +0900

    feat: localStorage 메뉴 불러오기 #28

--- 2022-11-25/js/index.js
+++ 2022-11-25/js/index.js
@@ -5,27 +5,21 @@ const store = {
         localStorage.setItem("menu", JSON.stringify(menu));
     },
     getLocalStorage() {
-        localStorage.getItem("menu");
+        return JSON.parse(localStorage.getItem("menu"));
     }
 }
 
 function App() {
     this.menu = [];
-
-    const updateMenuCount = () => {
-        const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
-        $('.menu-count').innerText = `총 ${menuCount}개`;
+    this.init = () => {
+        if (store.getLocalStorage().length > 1) {
+            this.menu = store.getLocalStorage();
+            console.log(this.menu);
+        }
+        render();
     }
 
-    const addMenuName = () => {
-        if ($('#espresso-menu-name').value === '') {
-            alert('이름을 입력해 주세요.');
-            return;
-        }
-
-        const espressoMenuName = $('#espresso-menu-name').value;
-        this.menu.push({name: espressoMenuName});
-        store.setLocalStorage(this.menu);
+    const render = () => {
         const template = this.menu
             .map((item, index) => {
                 return `
@@ -49,6 +43,23 @@ function App() {
 
         $('#espresso-menu-list').innerHTML = template;
         updateMenuCount();
+    }
+
+    const updateMenuCount = () => {
+        const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+        $('.menu-count').innerText = `총 ${menuCount}개`;
+    }
+
+    const addMenuName = () => {
+        if ($('#espresso-menu-name').value === '') {
+            alert('이름을 입력해 주세요.');
+            return;
+        }
+
+        const espressoMenuName = $('#espresso-menu-name').value;
+        this.menu.push({name: espressoMenuName});
+        store.setLocalStorage(this.menu);
+        render();
         $('#espresso-menu-name').value = '';
     }
 
@@ -96,4 +107,5 @@ function App() {
     });
 }
 
-new App();
+const app = new App();
+app.init();
